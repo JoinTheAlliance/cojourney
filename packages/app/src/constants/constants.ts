2ad353a0a3df6c646ff696ae412858a608d099d1
moon <shawmakesmagic@gmail.com>
2024-03-15 23:27:55 -0700

    add .env example

--- packages/app/src/constants/constants.ts
+++ packages/app/src/constants/constants.ts
@@ -10,8 +10,8 @@ export default {
   avatarPlaceholder: (seed: string | number) => {
     return `https://api.dicebear.com/6.x/micah/svg?seed=${seed}`
   },
-  supabaseUrl: "https://pronvzrzfwsptkojvudd.supabase.co",
-  supabaseAnonKey:
+  supabaseUrl: import.meta.env.VITE_SUPABASE_URL ?? "https://pronvzrzfwsptkojvudd.supabase.co",
+  supabaseAnonKey: import.meta.env.VITE_SUPABASE_ANON_KEY ??
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByb252enJ6ZndzcHRrb2p2dWRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY4NTYwNDcsImV4cCI6MjAyMjQzMjA0N30.I6_-XrqssUb2SWYg5DjsUqSodNS3_RPoET3-aPdqywM",
     posthogApiKey: "phc_zI9MsoTRSrvQoMSHj2lEdlzXN51nbOrdpS5ZUmWGWuU"
 }
