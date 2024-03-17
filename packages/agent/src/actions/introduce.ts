92b5f875d53b930abce6b3c85f83551a5bf11a3f
alextitonis <artificialleap12@gmail.com>
2024-03-17 13:12:42 +0300

    user-location-search

--- packages/agent/src/actions/introduce.ts
+++ packages/agent/src/actions/introduce.ts
@@ -1,13 +1,18 @@
-import { type UUID } from 'crypto'
+import { type UUID } from "crypto";
 import {
-  type BgentRuntime, composeContext, formatActors, getActorDetails, createRelationship, type Action,
+  type BgentRuntime,
+  composeContext,
+  formatActors,
+  getActorDetails,
+  createRelationship,
+  type Action,
   type ActionExample,
   type Memory,
   type Message,
   type State,
   parseJSONObjectFromText,
-  addHeader
-} from 'bgent'
+  addHeader,
+} from "bgent";
 
 export const template = `TASK: Introduce {{senderName}} to someone from {{agentName}}'s rolodex.
 The goal of this task is to determine which person from {{agentName}}'s rolodex would be the best match for {{senderName}} to meet.
@@ -46,83 +51,108 @@ Only respond with one connection.
 The response format should include userA, userB and explanation fields, and should be wrapped in a JSON block formatted for markdown with this structure:
 \`\`\`json
 { "explanation": Brief explanation of why they should be connected>, "userA": <name>, "userB": <name> }
-\`\`\``
+\`\`\``;
 
 export const getRelevantRelationships = async (
   runtime: BgentRuntime,
   message: Message,
   count: number = 5
 ) => {
+  const userId = message.senderId;
+  const userData = await runtime.databaseAdapter.getAccountById(userId);
+  const userDetails = userData?.details;
+  let searchEmbeddings: number[][] = [];
+  if (userDetails) {
+    const jsonDetails = JSON.parse(userDetails);
+    const userLocation = jsonDetails.location;
+    const embedding = await runtime.embed(userLocation);
+    searchEmbeddings.push(embedding);
+  }
+
   // Check if the user has a profile
   const descriptions = await runtime.descriptionManager.getMemoriesByIds({
-    userIds: [message.senderId, message.agentId]
-  })
+    userIds: [message.senderId, message.agentId],
+  });
   // if they dont, return empty string
   if (descriptions.length === 0) {
-    throw new Error('User does not have a profile')
+    throw new Error("User does not have a profile");
   }
   // if they do, run the rolodex match and return a list of good matches for them
-  const description = descriptions[0] as Memory
-  const searchEmbedding = description.embedding as number[]
+  const description = descriptions[0] as Memory;
+  console.log("description", description);
+  const searchEmbedding = description.embedding as number[];
+  searchEmbeddings.push(searchEmbedding);
 
-  const otherPeopleDescriptions = (
-    await runtime.descriptionManager.searchMemoriesByEmbedding(
-      searchEmbedding,
-      {
-        match_threshold: 0,
-        count: count + 1,
-        unique: true
-      }
-    )
-  )
-    .filter((d: Memory) => d.user_id !== message.senderId && d.user_id !== message.agentId)
-    // .slice(0, count)
+  const otherPeopleDescriptions = [];
+  for (let i = 0; i < searchEmbeddings.length; i++) {
+    const otherPeopleDescriptionsSub = (
+      await runtime.descriptionManager.searchMemoriesByEmbedding(
+        searchEmbedding,
+        {
+          match_threshold: 0,
+          count: count + 1,
+          unique: true,
+        }
+      )
+    ).filter(
+      (d: Memory) =>
+        d.user_id !== message.senderId && d.user_id !== message.agentId
+    );
+    otherPeopleDescriptions.push(...otherPeopleDescriptionsSub);
+  }
+
+  //clear duplicates
+  const uniqueOtherPeopleDescriptions = Array.from(
+    new Set(otherPeopleDescriptions)
+  );
+
+  // .slice(0, count)
 
   // get all the userIds from the user_ids of the otherPeopleDescriptions, make sure unique
   const userIds = Array.from(
-    new Set(otherPeopleDescriptions.map((d: Memory) => d.user_id as UUID))
-  )
+    new Set(uniqueOtherPeopleDescriptions.map((d: Memory) => d.user_id as UUID))
+  );
 
   const actorsData = await getActorDetails({
     runtime,
-    userIds
-  })
+    userIds,
+  });
 
-  const formattedActorData = formatActors({ actors: actorsData })
+  const formattedActorData = formatActors({ actors: actorsData });
 
-  return addHeader('## ROLODEX', formattedActorData)
-}
+  return addHeader("## ROLODEX", formattedActorData);
+};
 
 const handler = async (runtime: BgentRuntime, message: Message) => {
-  const state = (await runtime.composeState(message)) as State
+  const state = (await runtime.composeState(message)) as State;
   const relevantRelationships = await getRelevantRelationships(
     runtime,
     message
-  )
-  console.log('***** relevantRelationships')
-  console.log(relevantRelationships)
+  );
+  console.log("***** relevantRelationships");
+  console.log(relevantRelationships);
 
   const context = composeContext({
     state: { ...state, relevantRelationships },
-    template
-  })
+    template,
+  });
 
-  let responseData = null
+  let responseData = null;
   for (let triesLeft = 3; triesLeft > 0; triesLeft--) {
     const response = await runtime.completion({
       context,
-      stop: []
-    })
+      stop: [],
+    });
 
-    const parsedResponse = parseJSONObjectFromText(response)
+    const parsedResponse = parseJSONObjectFromText(response);
 
     if (parsedResponse) {
-      responseData = parsedResponse
-      break
+      responseData = parsedResponse;
+      break;
     }
 
     if (runtime.debugMode) {
-      console.log(`introduceHandler response: ${response}`)
+      console.log(`introduceHandler response: ${response}`);
     }
   }
 
@@ -130,156 +160,156 @@ const handler = async (runtime: BgentRuntime, message: Message) => {
     await createRelationship({
       runtime,
       userA: responseData.userA,
-      userB: responseData.userB
-    })
+      userB: responseData.userB,
+    });
   } else if (runtime.debugMode) {
-    console.log('No connection made, could not parse response')
+    console.log("No connection made, could not parse response");
   }
-}
+};
 
 export const action = {
-  name: 'INTRODUCE',
+  name: "INTRODUCE",
   validate: async (
     runtime: BgentRuntime,
     message: Message
   ): Promise<boolean> => {
     // Retrieve descriptions for the message sender
     const descriptions = await runtime.descriptionManager.getMemoriesByIds({
-      userIds: [message.senderId]
-    })
+      userIds: [message.senderId],
+    });
 
     // If the sender has at least one description, return true to indicate the action is valid
-    return descriptions.length > 0
+    return descriptions.length > 0;
   },
   description:
-    'Introduce the user to someone from the rolodex who they might like to chat with. Only use this if the user is expressing interest in meeting someone new. If the user has not expressed interest, DO NOT USE THIS ACTION.',
+    "Introduce the user to someone from the rolodex who they might like to chat with. Only use this if the user is expressing interest in meeting someone new. If the user has not expressed interest, DO NOT USE THIS ACTION.",
   handler,
   condition:
-    'The agent wants to introduce the user to someone from the rolodex',
+    "The agent wants to introduce the user to someone from the rolodex",
   examples: [
     [
       {
-        user: '{{user1}}',
+        user: "{{user1}}",
         content: {
           content:
             "I've been wanting to meet someone who's into indie music like I am.",
-          action: 'WAIT'
-        }
+          action: "WAIT",
+        },
       },
       {
-        user: '{{agent}}',
+        user: "{{agent}}",
         content: {
           content:
-            'I know just the person! Let me introduce you to Alex, who is a huge indie music fan.',
-          action: 'INTRODUCE'
-        }
+            "I know just the person! Let me introduce you to Alex, who is a huge indie music fan.",
+          action: "INTRODUCE",
+        },
       },
       {
-        user: '{{agent}}',
+        user: "{{agent}}",
         content: {
           content:
             "I've sent Alex a message to see if they're up for a chat. Hang tight!",
-          action: 'WAIT'
-        }
-      }
+          action: "WAIT",
+        },
+      },
     ],
     [
       {
-        user: '{{user1}}',
+        user: "{{user1}}",
         content: {
           content:
             "I'm trying to expand my professional network in the graphic design field.",
-          action: 'WAIT'
-        }
+          action: "WAIT",
+        },
       },
       {
-        user: '{{agent}}',
+        user: "{{agent}}",
         content: {
           content:
             "Great! I'll introduce you to Jordan, who is well-connected in the graphic design community.",
-          action: 'INTRODUCE'
-        }
+          action: "INTRODUCE",
+        },
       },
       {
-        user: '{{agent}}',
+        user: "{{agent}}",
         content: {
           content:
             "Jordan is usually quick to respond. Let's give them a moment.",
-          action: 'WAIT'
-        }
-      }
+          action: "WAIT",
+        },
+      },
     ],
 
     [
       {
-        user: '{{user1}}',
+        user: "{{user1}}",
         content: {
           content:
             "I'm quite busy these days with work and barely have time for hobbies.",
-          action: 'WAIT'
-        }
+          action: "WAIT",
+        },
       },
       {
-        user: '{{agent}}',
+        user: "{{agent}}",
         content: {
           content:
-            'Understood. If you ever want to meet someone with similar interests, let me know!',
-          action: 'WAIT'
-        }
-      }
+            "Understood. If you ever want to meet someone with similar interests, let me know!",
+          action: "WAIT",
+        },
+      },
     ],
     [
       {
-        user: '{{user1}}',
+        user: "{{user1}}",
         content: {
           content: "I've heard that chess is becoming popular again.",
-          action: 'WAIT'
-        }
+          action: "WAIT",
+        },
       },
       {
-        user: '{{agent}}',
+        user: "{{agent}}",
         content: {
           content:
             "Yes, it's quite fascinating how it's regained popularity. If you're ever interested in meeting fellow chess enthusiasts, feel free to tell me!",
-          action: 'WAIT'
-        }
-      }
+          action: "WAIT",
+        },
+      },
     ],
 
     [
       {
-        user: '{{user1}}',
+        user: "{{user1}}",
         content: {
           content: "I'm not sure if I'm ready to meet new people yet.",
-          action: 'WAIT'
-        }
+          action: "WAIT",
+        },
       },
       {
-        user: '{{agent}}',
+        user: "{{agent}}",
         content: {
           content:
             "No worries at all. Take your time, and I'm here when you're ready.",
-          action: 'WAIT'
-        }
-      }
+          action: "WAIT",
+        },
+      },
     ],
     [
       {
-        user: '{{user1}}',
+        user: "{{user1}}",
         content: {
           content:
-            'Maybe meeting someone new would be nice. Oh, did you see the latest football match?',
-          action: 'WAIT'
-        }
+            "Maybe meeting someone new would be nice. Oh, did you see the latest football match?",
+          action: "WAIT",
+        },
       },
       {
-        user: '{{agent}}',
+        user: "{{agent}}",
         content: {
           content:
             "I did catch the highlights! If you decide you'd like to meet someone from the community, just let me know.",
-          action: 'WAIT'
-        }
-      }
-    ]
-  ] as ActionExample[][]
-} as Action
+          action: "WAIT",
+        },
+      },
+    ],
+  ] as ActionExample[][],
+} as Action;
